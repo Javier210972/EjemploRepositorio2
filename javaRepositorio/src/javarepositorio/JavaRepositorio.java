6160bf5b51bc36b62d69d102f5c0791b6588659c
javam2018 <javam2018@s10-96637.redes.local>
2018-07-12 11:06:19 -0600

    Cambio 2

--- javaRepositorio/src/javarepositorio/JavaRepositorio.java
+++ javaRepositorio/src/javarepositorio/JavaRepositorio.java
@@ -16,6 +16,7 @@ public class JavaRepositorio {
      */
     public static void main(String[] args) {
         System.out.println("Guatemala");
+        System.out.println("prueba");
     }
     
 }
